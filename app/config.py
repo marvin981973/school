1cd92f2e37a80a1d4780dbbbd7a4c0dfe5a6fc01
xing-shuai <shuaihongjie@outlook.com>
2018-04-14 23:16:41 +0800

    config modify

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:187125@localhost/school"
     SECRET_KEY = 'school'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
     def init_app(app):
